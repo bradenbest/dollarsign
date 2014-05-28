4b17b1dea9721cacdde6a8d8fb58404e028bb9a6
Braden Best <bradenbest@users.noreply.github.com>
2014-05-27 19:03:47 -0600

    refactoring

--- src/dollar.bash
+++ src/dollar.bash
@@ -1,11 +1,16 @@
 #!/bin/bash
 # For people who are too literal to realize that '$' is not part of the command they're being told to copy/paste. 
 
-args=""
+USER_SHAME=0
+COMMAND=""
+
 for arg in $@; do
-  args="$args $arg"
+  COMMAND="$COMMAND $arg"
 done
 
-echo "Just so you know, you weren't supposed to copy the '$', too! I'll run it anyway, but next time, focus, alright?"
-read -p "Hit enter to continue"
-$args
+if [ $USER_SHAME == "1" ] ; then
+  echo "Just so you know, you weren't supposed to copy the '$', too! I'll run it anyway, but next time, focus, alright?"
+  read -p "Hit enter to continue"
+fi
+
+$COMMAND
