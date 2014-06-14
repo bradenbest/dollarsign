8466d83a1ae46e1cf8dbb2708d162e4698cba588
bradenbest <bradentbest@gmail.com>
2014-06-14 06:48:01 -0600

    Removed some redundancy
    Looping through $@ to generate the COMMAND variable without realizing
    that $COMMAND == $@.

--- src/dollar.bash
+++ src/dollar.bash
@@ -2,15 +2,10 @@
 # For people who are too literal to realize that '$' is not part of the command they're being told to copy/paste. 
 
 USER_SHAME=0
-COMMAND=""
-
-for arg in $@; do
-  COMMAND="$COMMAND $arg"
-done
 
 if [ $USER_SHAME == "1" ] ; then
   echo "Just so you know, you weren't supposed to copy the '$', too! I'll run it anyway, but next time, focus, alright?"
   read -p "Hit enter to continue"
 fi
 
-$COMMAND
+$@
